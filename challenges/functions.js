9b40858095e5f93c0a081fe808ba95bc036b6be5
Matthew John Orth <matthewjohnorth@Matthews-MacBook-Pro-72.local>
2019-08-18 19:40:55 -0700

    functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, cb) {
+  return cb(arg1, arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,23 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+};
+
+function multiply(num1, num2) {
+  return num1 * num2;
+};
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+};
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +42,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// When a function doesn't have a variable defined within its scope, it will look to the outer functions scope for context. This is possible because functions are hoisted on the first run through of the compiler.
 
 
 const external = "I'm outside the function";
